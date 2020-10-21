96cb278ae5a21248984ea6fce0a31654fbe8edd8
Brian Parker <btparker70@gmail.com>
2020-10-20 18:08:38 -0700

    added highscores page, trying to get highscores workin

--- assets/javascript/highscores.js
+++ assets/javascript/highscores.js
@@ -1,7 +1,39 @@
+var firstNameInput = document.querySelector("#first-name");
+var submitBtn = document.querySelector("#submit");
+var userFirstNameSpan = document.querySelector("#user-first-name");
+
 document.getElementById("quizBtn").onclick = function () {
     location.href = "index.html";
 }
 
-var test = localStorage.getItem('high score');
 
-document.querySelector("#answer1").innerHTML = test;
+
+submitBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    var user = {
+        firstName: firstNameInput.value.trim(),
+      };
+
+      console.log(user);
+      localStorage.setItem("user", user);
+
+      var lastUser = localStorage.getItem("user");
+      userFirstNameSpan.textContent = lastUser.name;
+
+     
+})
+
+
+
+
+
+
+// document.querySelector("#highScore1").innerHTML = test;
+
+//var lastUser = localStorage.getItem('high score');
+
+// document.getElementById('form').addEventListener('submit', function(event){
+//     event.preventDefault();
+//     // var formText = 
+// })
